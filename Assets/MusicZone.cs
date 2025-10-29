97be97dcb9d1ebe4925e13f185287ca8bb5a0d4f
gorejiraa <gorejiraa@gmail.com>
2025-10-29 11:22:08 -0300

    Added Audio Source, Fixed Script, Added Trigger Zone
    We got Miku Radio on the go now - semi blue transparent square as trigger on a lil table for an old FM radio.

--- Assets/MusicZone.cs
+++ Assets/MusicZone.cs
@@ -1,25 +1,30 @@
 using UnityEngine;
 
 public class MusicZone : MonoBehaviour
-
+{
     public AudioSource musicSource;
+    private bool isPlaying = false;
 
     private void OnTriggerEnter(Collider other)
-    
+    {
         if (other.CompareTag("Player"))
         {
-            musicSource.Pause(); 
-            Debug.Log("Music paused.");
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    
-        if (other.CompareTag("Player"))
-        {
-            musicSource.UnPause(); 
-            Debug.Log("Music unpaused.");
+            if (isPlaying)
+            {
+             
+                musicSource.Pause();
+                Debug.Log("Music paused.");
+                isPlaying = false;
+            }
+            else
+            {
+               
+                musicSource.UnPause();
+                if (!musicSource.isPlaying)
+                    musicSource.Play(); 
+                Debug.Log("Music playing.");
+                isPlaying = true;
+            }
         }
     }
 }
-
